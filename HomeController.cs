340a33de08ed73e67e01e0bc5d7355b59ad9d119
Miguel Bacellar <77118813+yEIeven@users.noreply.github.com>
2024-08-22 16:25:22 -0300

    Arquivos do meu site

---
+++ HomeController.cs
@@ -0,0 +1,55 @@
+﻿using BlogCurriculo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogCurriculo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Curriculo()
+        {
+            return View();
+        }
+        public IActionResult Projeto()
+        {
+            return View();
+        }
+        public IActionResult Sobremim()
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ site.js
@@ -0,0 +1,75 @@
+﻿
+
+const mobile = document.querySelector(".mobile");
+const backMobile = document.querySelector(".backMobile");
+
+mobile.addEventListener("click", () => backMobile.classList.toggle("active"));
+
+
+
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["C#", "AspNet", "JavaScript", "FullStack"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.getElementById('tema');
+
+    // Verificar se o tema está salvo no localStorage
+    if (localStorage.getItem('tema') === 'escuro') {
+        document.body.classList.add('tema-escuro');
+        checkbox.checked = true;
+    }
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.body.classList.add('tema-escuro');
+            localStorage.setItem('tema', 'escuro');
+        } else {
+            document.body.classList.remove('tema-escuro');
+            localStorage.setItem('tema', 'claro');
+        }
+    });
+});      
